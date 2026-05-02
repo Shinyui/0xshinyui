7c94f9ccd01e15417e9919a274d9f7fb4c73cd18
Shinyui Chu <shinyui@Shinyuis-MacBook-Air.local>
2026-05-02 22:11:22 +0800

    update sitemap

--- src/lib/siteConfig.ts
+++ src/lib/siteConfig.ts
@@ -1,5 +1,5 @@
 export const siteConfig = {
-  siteUrl: 'https://0xshinyui.xyz',
+  siteUrl: 'https://www.0xshinyui.xyz',
   siteName: '0xShinyui 技術部落格',
   locale: 'zh-TW',
   defaultDescription: '分享產品管理、技術開發、運營經驗的部落格',
